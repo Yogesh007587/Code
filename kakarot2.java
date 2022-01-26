c3d13f02efc6c98fd5ac0cde05960f0176c866da
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-01-26 22:03:23 +0530

    Create kakarot2.java

---
+++ kakarot2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class v {
+    public static void main(String[] args) {
+
+        Scanner a = new Scanner(System.in);
+
+        System.out.print("Enter your name Sir/Madam :-");
+        String c = a.next();
+        System.out.println("Hello " + c + " have a good day");
+    }
+}
