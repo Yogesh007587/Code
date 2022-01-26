9a267e1e9abc17ee6433f589af94a9a81e3418c8
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-01-26 15:30:06 +0530

    Create kooky1.java

---
+++ kooky1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.net.SocketOption;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        int number1=input.nextInt();
+        int number2=input.nextInt();
+        int number3=input.nextInt();
+
+        int sum = number1 + number2 +number3 ;
+        System.out.println(sum);
+
