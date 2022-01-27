9de6454d0509f635fa1819fa58ad2e7a779878ed
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-01-27 13:33:54 +0530

    Create grade.java

---
+++ grade.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class va {
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+    }
+}
