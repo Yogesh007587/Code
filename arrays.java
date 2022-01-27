4aa3aca8fee19fee6c1b545c6093d22c6ee87af5
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-01-28 00:33:45 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class arrays {
+    public static void main(String[] args) {
+        int[] marks ={40,30,60,70,99};
+        for(int i=marks.length-1;i>=0;i--){
+            System.out.println(marks[i]);
+        }
+
+    }
+}
