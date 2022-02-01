5c4de23a4cdce256a721bce77fbf21fab9f1bcf9
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-02-02 02:34:17 +0530

    Create cylinder_with_constructor.java

---
+++ cylinder_with_constructor.java
@@ -0,0 +1,45 @@
+package com.company;
+
+class cylinder{
+    private int radius ;
+    private int height ;
+    public cylinder(int radius , int height){
+        this.radius = radius;
+        this.height = height;
+    }
+/*
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }*/
+    public double volume(){
+        return 3.14*radius*radius*height;
+    }
+    public double surfacearea(){
+        return 2*3.14*radius*height + 2*3.14*radius*radius;
+    }
+}
+public class psa {
+    public static void main(String[] args) {
+        cylinder c = new cylinder(12,9);
+
+       /* c.setRadius(5);
+        c.setHeight(10);
+        System.out.println(c.getHeight());
+        System.out.println(c.getRadius());*/
+        System.out.println(c.volume());
+        System.out.println(c.surfacearea());
+
+    }
+}
