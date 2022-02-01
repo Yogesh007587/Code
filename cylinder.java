e8aeb60fcaed65011c8916a169f7b8addd4d4b99
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-02-02 02:17:26 +0530

    Create cylinder.java

---
+++ cylinder.java
@@ -0,0 +1,31 @@
+package com.company;
+
+class cylinder{
+    private int radius ;
+    private int height ;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
+public class psa {
+    public static void main(String[] args) {
+        cylinder c = new cylinder();
+        c.setRadius(5);
+        c.setHeight(10);
+        System.out.println(c.getHeight());
+        System.out.println(c.getRadius());
+    }
+}
