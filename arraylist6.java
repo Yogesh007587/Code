0ebf756230e9bb72dd4dcfac663407b994e0b6c1
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-04-02 20:45:44 +0530

    Create arraylist6.java

---
+++ arraylist6.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class yogi1
+{
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		// add elemets
+        list.add(0);
+		list.add(1);
+		list.add(2);
+
+        //System.out.println(list);
+
+        //get elements
+        //System.out.println(list.get(0));
+
+        //add elements in between
+        list.add(1, 1);
+        //System.out.println(list);
+
+        //set elements
+        list.set(0, 5);
+        //System.out.println(list);
+
+        //remove elements
+        list.remove(0);
+        //System.out.println(list);
+
+        //size
+        int size = list.size();
+        System.out.println(size);
+	}
+}
