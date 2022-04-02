b42a3202319ca84a1bd490b2f9b4d498f6719eee
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-04-02 20:36:03 +0530

    Create arraylist3.java

---
+++ arraylist3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class yogi1
+{
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		// add elemets
+        list.add(0);
+		list.add(1);
+		list.add(2);
+
+        //System.out.println(list);
+
+        //get elements
+        //System.out.println(list.get(0));
+
+        //add elements in between
+        list.add(1, 1);
+        System.out.println(list);
+	}
+}
