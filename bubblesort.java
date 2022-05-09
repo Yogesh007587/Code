35e8fe8a89bbc08609421f8ede0e394708b8fe08
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-05-09 23:19:17 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.*;
+public class sorting {
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {3, 6, 8, 1, 2};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1]=temp;
+                }
+            }
+        }
+        print(arr);
+    }
+}
+
+
